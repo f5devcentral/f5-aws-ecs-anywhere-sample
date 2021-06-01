83641a8d60f3703145538c21b17299574d7740d5
Eric Chen <eric.chen@f5.com>
2021-06-01 19:35:57 -0400

    randomize name of secret

--- terraform/secret.tf
+++ terraform/secret.tf
@@ -1,6 +1,10 @@
-resource "aws_secretsmanager_secret" "bigip_password" {
-  name = "${var.prefix}-bigip_password"
+resource "random_id" "id" {
+  byte_length = 2
 }
+resource "aws_secretsmanager_secret" "bigip_password" {
+  name = format("%s-bigip-password-%s", var.prefix, random_id.id.hex)
+}
+
 resource "aws_secretsmanager_secret_version" "bigip_password" {
   secret_id     = aws_secretsmanager_secret.bigip_password.id
   secret_string = var.bigip_password
